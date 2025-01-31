f071ba6931bc3007b035af7b8b489bface8879ae
aakashdarda290706 <aakashdarda290706@gmail.com>
2025-01-31 15:12:37 +0530

    Update settings.py

--- studypalz/settings.py
+++ studypalz/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'sp.urls'
+ROOT_URLCONF = 'studypalz.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'sp.wsgi.application'
+WSGI_APPLICATION = 'studypalz.wsgi.application'
 
 
 # Database
